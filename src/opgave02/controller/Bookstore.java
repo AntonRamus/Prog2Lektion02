306f673ac26bb37d78d12b23d593ca2763f0adf1
Anton Ramus <anton.ramus@hotmail.com>
2025-02-03 10:41:27 +0100

    procent ændret til negativ

--- src/opgave02/controller/Bookstore.java
+++ src/opgave02/controller/Bookstore.java
@@ -8,7 +8,7 @@ public class Bookstore {
         Basket basket = new Basket();
         basket.addItem(new Book(600, "Introduction to Java Programming and Data Structures"));
         basket.addItem(new Book(350, "Head First Design Patterns"));
-        basket.addDiscount(new ProcentDiscount("10% rabat på alt", 10));
+        basket.addDiscount(new ProcentDiscount("10% rabat på alt", -10));
         basket.addDiscount(new FixedDiscount("25 kr rabat på beløb over 500 kr.", -25, 500));
         basket.addDiscount(new FixedDiscount("50 kr rabat på beløb over 1000 kr.",-50, 1000));
         basket.printTotal();
