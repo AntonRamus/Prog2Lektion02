b6de19b676fd8504e6edaadd954e3a93ca068f67
Anton Ramus <anton.ramus@hotmail.com>
2025-02-03 17:48:40 +0100

    flere bøger

--- src/opgave02/controller/Bookstore.java
+++ src/opgave02/controller/Bookstore.java
@@ -8,6 +8,8 @@ public class Bookstore {
         Basket basket = new Basket();
         basket.addItem(new EBook(600, "Introduction to Java Programming and Data Structures"));
         basket.addItem(new Book(350, "Head First Design Patterns"));
+        basket.addItem(new Book(450, "Hands on machine learning"));
+        basket.addItem(new EBook(200, "SQL in 10 minutes a day"));
         basket.addDiscount(new ProcentDiscount("10% rabat på alt.", -10));
         basket.addDiscount(new EBookProcentDiscount("5% rabat på e-bøger", -5));
         basket.addDiscount(new FixedDiscount("25 kr rabat på beløb over 500 kr.", -25, 500));
