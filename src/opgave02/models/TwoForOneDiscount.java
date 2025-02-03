d9a0a34ba831a08fbcbc9c43a7e4d500f6c7b339
Anton Ramus <anton.ramus@hotmail.com>
2025-02-03 17:48:29 +0100

    ekstra opgave på 2for1 rabat

--- src/opgave02/models/TwoForOneDiscount.java
+++ src/opgave02/models/TwoForOneDiscount.java
@@ -1,5 +1,7 @@
 package opgave02.models;
 
+import java.util.ArrayList;
+
 public class TwoForOneDiscount extends Discount {
     private final String description;
 
@@ -9,19 +11,44 @@ public class TwoForOneDiscount extends Discount {
 
     @Override
     public double applyDiscount(Basket basket) {
-        Book cheapestBook = basket.getItems().getFirst();
-        if (basket.getItems().size() > 1) {
-            for (int i = 1; i < basket.getItems().size(); i++) {
-                if (cheapestBook.getPrice() > basket.getItems().get(i).getPrice()) {
-                    cheapestBook = basket.getItems().get(i);
-                }
-            }
+        int numberOfDiscountedBooks = basket.getItems().size() / 2;
+        double totalDiscount = 0;
+        ArrayList<Book> sortedItems = selectionSort(basket.getItems());
+        for (int i = 0; i < numberOfDiscountedBooks; i++) {
+            totalDiscount -= sortedItems.get(i).getPrice();
         }
-        return -cheapestBook.getPrice();
+        return totalDiscount;
     }
 
     @Override
     public String getDescription() {
         return description;
+    }
+
+    private ArrayList<Book> selectionSort(ArrayList<Book> books) {
+        ArrayList<Book> bookArray = books;
+        int length = bookArray.size();
+
+        for (int i = 0; i < length - 1; i++) {
+            Book cheapestBook = bookArray.get(i);
+            int index = i;
+            for (int j = i + 1; j < length; j++) {
+                if (bookArray.get(j).getPrice() < cheapestBook.getPrice()) {
+                    cheapestBook = bookArray.get(j);
+                    index = j;
+                }
+            }
+            if (i != index) {
+                bookArray = swap(bookArray, i, index);
+            }
+        }
+        return bookArray;
+    }
+
+    private ArrayList<Book> swap(ArrayList<Book> bookArray, int index, int indexOfMin) {
+        Book book = bookArray.get(index);
+        bookArray.set(index, bookArray.get(indexOfMin));
+        bookArray.set(indexOfMin, book);
+        return bookArray;
     }
 }
